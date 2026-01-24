7a22a5119d12052c03f49e0cdc3e7b01b62eb10b
NCHANDOMALPHAELIAS <nchandoms@gmail.com>
2026-01-24 20:09:22 +0100

    layout updates

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -26,7 +26,6 @@
                         class="h-10 max-lg:hidden [&>div>svg]:size-5"
                         icon="book-open-text"
                         href="/"
-                        wire:navigate
                         label="Go back to Website"
                     />
                 </flux:tooltip>
@@ -53,11 +52,8 @@
             <flux:spacer />
 
             <flux:sidebar.nav>
-                <flux:sidebar.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                    {{ __('Repository') }}
-                </flux:sidebar.item>
-                <flux:sidebar.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                    {{ __('Documentation') }}
+                <flux:sidebar.item icon="book-open-text" href="/">
+                    {{ __('Back to website') }}
                 </flux:sidebar.item>
             </flux:sidebar.nav>
         </flux:sidebar>
