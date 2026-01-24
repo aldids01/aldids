9ba8c6de407f584c2fe170227ea8140c9e9be1f5
NCHANDOMALPHAELIAS <nchandoms@gmail.com>
2026-01-24 19:56:46 +0100

    login form information updated

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -51,7 +51,7 @@
 
             <div class="space-x-1 text-sm text-center rtl:space-x-reverse text-zinc-600 dark:text-zinc-400">
 {{--                <span>{{ __('Don\'t have an account?') }}</span>--}}
-                <flux:link :href="/" wire:navigate>{{ __('Back to website') }}</flux:link>
+                <flux:link href="/" wire:navigate>{{ __('Back to website') }}</flux:link>
             </div>
     </div>
 </x-layouts::auth>
