95353da4595c68cc4d04883eef8e0f675602eea3
NCHANDOMALPHAELIAS <nchandoms@gmail.com>
2026-01-24 19:55:19 +0100

    login form updated

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -49,11 +49,9 @@
             </div>
         </form>
 
-        @if (Route::has('register'))
             <div class="space-x-1 text-sm text-center rtl:space-x-reverse text-zinc-600 dark:text-zinc-400">
 {{--                <span>{{ __('Don\'t have an account?') }}</span>--}}
                 <flux:link :href="/" wire:navigate>{{ __('Back to website') }}</flux:link>
             </div>
-        @endif
     </div>
 </x-layouts::auth>
