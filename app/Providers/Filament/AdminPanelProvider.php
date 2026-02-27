1ad5b47fb0d28e54209e86ca5a27cc8b072555a2
NCHANDOMALPHAELIAS <nchandoms@gmail.com>
2026-02-27 18:23:33 +0100

    registration link remove

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,7 +33,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->login()
-            ->registration()
+//            ->registration()
             ->colors([
                 'primary' => Color::Purple,
             ])
